aaa7f380a5b966b4e0b91bf62dfa3ca2c862e01d
pepealdana <pepealdanagomez@hotmail.com>
2026-06-25 00:51:02 -0500

    reemplaza route.js

--- js/router.js
+++ js/router.js
@@ -1,3 +1,34 @@
-function navigate(route){
-    window.location.hash = route;
+function navegar(ruta) {
+
+    switch(ruta) {
+
+        case "inicio":
+            mostrarInicio();
+            break;
+
+        case "biblioteca":
+            mostrarBiblioteca();
+            break;
+
+        case "favoritas":
+            mostrarFavoritas();
+            break;
+
+        case "progreso":
+            mostrarProgreso();
+            break;
+
+        case "configuracion":
+            mostrarConfiguracion();
+            break;
+
+        case "acerca":
+            mostrarAcerca();
+            break;
+
+        default:
+            mostrarInicio();
+
+    }
+
 }
