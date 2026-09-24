0be0959ad99424fdcfb6e1cf7c29c7e5df9a154d
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-23 22:25:06 -0500

    Actualizar caché para corregir archivos recientes

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v5";
+const CACHE_NAME = "amigos-del-cielo-v6";
 
 const APP_SHELL = [
     "./",
