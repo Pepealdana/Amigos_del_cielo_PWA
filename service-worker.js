a306b98aaad52f954064ed5079f7fefd89a0d635
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-24 12:39:11 -0500

    Actualizar cache para Santa Teresita de Lisieux

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v35";
+const CACHE_NAME = "amigos-del-cielo-v36";
 
 const APP_SHELL = [
     "./",
@@ -32,6 +32,7 @@ const APP_SHELL = [
     "./data/santa-rita-de-casia.json",
     "./data/santa-rosa-de-lima.json",
     "./data/santa-teresa-de-avila.json",
+    "./data/santa-teresita-de-lisieux.json",
     "./css/style.css",
     "./css/01-base.css",
     "./css/02-layout.css",
