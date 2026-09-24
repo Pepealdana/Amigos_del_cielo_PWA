29dcc5b8926dc2654e9678276db8691524fed02d
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-24 09:32:05 -0500

    Actualiza caché para mostrar historias ampliadas

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v30";
+const CACHE_NAME = "amigos-del-cielo-v31";
 
 const APP_SHELL = [
     "./",
