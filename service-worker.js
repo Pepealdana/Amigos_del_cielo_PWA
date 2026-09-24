23169a61712bb8915911976853abd2b2c41d19e9
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-24 11:22:32 -0500

    chore: bump service worker cache to v34

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v33";
+const CACHE_NAME = "amigos-del-cielo-v34";
 
 const APP_SHELL = [
     "./",
