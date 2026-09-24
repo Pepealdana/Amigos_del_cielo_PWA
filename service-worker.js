4924feca78e1e0622afc6ae716bb0bb47a6a647d
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-24 09:03:02 -0500

    Actualiza caché para contenido revisado de Rita y Pío

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v28";
+const CACHE_NAME = "amigos-del-cielo-v29";
 
 const APP_SHELL = [
     "./",
