bdae8b9a6a8da9891c750733bda32d1df4e3fd1f
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-24 05:13:30 -0500

    Actualizar cache para San Ignacio de Loyola

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v13";
+const CACHE_NAME = "amigos-del-cielo-v14";
 
 const APP_SHELL = [
     "./",
@@ -15,6 +15,7 @@ const APP_SHELL = [
     "./data/san-agustin-de-hipona.json",
     "./data/san-benito-abad.json",
     "./data/san-francisco-de-asis.json",
+    "./data/san-ignacio-de-loyola.json",
     "./css/style.css",
     "./css/01-base.css",
     "./css/02-layout.css",
