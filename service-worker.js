685e782168a14c77d91ac6e7798d2093d897bb01
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-24 23:08:20 -0500

    Actualizar caché con la versión final del servicio de compartir

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v68";
+const CACHE_NAME = "amigos-del-cielo-v69";
 
 const APP_SHELL = [
     "./",
