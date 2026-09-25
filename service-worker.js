d2cdcb814c3ddbfe72461927d77a0265309a7453
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-24 19:12:32 -0500

    Bump cache after JSON line-break fixes

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v58";
+const CACHE_NAME = "amigos-del-cielo-v59";
 
 const APP_SHELL = [
     "./",
