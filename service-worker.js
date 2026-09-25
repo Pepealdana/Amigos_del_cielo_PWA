86415d1c8ed1c2351e18126868618dd121a3dae9
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-25 09:15:10 -0500

    chore(v1.2): actualizar cache PWA a v75

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v74";
+const CACHE_NAME = "amigos-del-cielo-v75";
 
 const APP_SHELL = [
     "./",
