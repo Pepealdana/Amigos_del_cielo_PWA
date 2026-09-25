830d88585fedf37d719b8a0bd122f6d6b10441e8
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-25 14:13:36 -0500

    Actualizar caché tras prioridad de novena activa

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v83";
+const CACHE_NAME = "amigos-del-cielo-v84";
 
 const APP_SHELL = [
     "./",
