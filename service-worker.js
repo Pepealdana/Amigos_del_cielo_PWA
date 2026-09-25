55ea616f7c2f93df03e89df6178ca0705b3dff1e
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-24 23:07:44 -0500

    Actualizar caché tras implementar compartir e instalación

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v67";
+const CACHE_NAME = "amigos-del-cielo-v68";
 
 const APP_SHELL = [
     "./",
