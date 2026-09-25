ce92e39d71397dcd6248971cc8360199897049e9
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-25 09:15:43 -0500

    chore(v1.2): incluir datos de expansion en cache

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v75";
+const CACHE_NAME = "amigos-del-cielo-v76";
 
 const APP_SHELL = [
     "./",
