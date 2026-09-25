08ff8e450661552f110b83b02f4fc097e226f241
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-25 15:05:09 -0500

    Actualizar caché por corrección de citas verificadas

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v91";
+const CACHE_NAME = "amigos-del-cielo-v92";
 
 const APP_SHELL = [
     "./",
