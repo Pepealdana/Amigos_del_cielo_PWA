6b1080a227d2abc01493072a16f5f9f1a850187c
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-25 13:13:44 -0500

    Actualizar caché tras normalización territorial v2

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v80";
+const CACHE_NAME = "amigos-del-cielo-v81";
 
 const APP_SHELL = [
     "./",
