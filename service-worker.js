5efb932c6168f8bc013fb8ad3fc64fda46f13e45
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-25 13:21:15 -0500

    Actualizar caché para modelo territorial v2.1

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v81";
+const CACHE_NAME = "amigos-del-cielo-v82";
 
 const APP_SHELL = [
     "./",
