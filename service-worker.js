eb68ccd532cbe6412851ede7df2d786bdf134548
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-24 22:20:43 -0500

    Actualizar caché tras ajuste de espaciado visual

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v64";
+const CACHE_NAME = "amigos-del-cielo-v65";
 
 const APP_SHELL = [
     "./",
