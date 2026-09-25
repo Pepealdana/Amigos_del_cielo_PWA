e979f92e0e488d3102f6a783471107fe436b8c1f
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-25 14:13:16 -0500

    Actualizar caché PWA tras mejoras de inicio y contenido

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v82";
+const CACHE_NAME = "amigos-del-cielo-v83";
 
 const APP_SHELL = [
     "./",
