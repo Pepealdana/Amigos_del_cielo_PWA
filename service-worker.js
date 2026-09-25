aa339793c98b5952f61385e53a380b406b156900
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-25 16:45:48 -0500

    chore: actualizar cache a v95 y agregar Virgen de Luján

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v94";
+const CACHE_NAME = "amigos-del-cielo-v95";
 
 const APP_SHELL = [
     "./",
@@ -63,6 +63,8 @@ const APP_SHELL = [
     "./data/santa-teresa-de-jesus-de-los-andes.json",
     "./data/san-alberto-hurtado.json",
     "./data/san-jose-gabriel-del-rosario-brochero.json",
+    "./data/santa-carmen-rendiles-martinez.json",
+    "./data/virgen-de-lujan.json",
     "./data/santa-carmen-rendiles-martinez.json",
     "./css/style.css",
     "./css/01-base.css",
