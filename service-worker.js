e1f637cae157b97115c98dae4de719994ec44ab8
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-25 17:52:20 -0500

    Actualizar caché PWA a v113 y agregar Nuestra Señora de los Desamparados

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v112";
+const CACHE_NAME = "amigos-del-cielo-v113";
 
 const APP_SHELL = [
     "./",
@@ -82,6 +82,7 @@ const APP_SHELL = [
     "./data/virgen-de-los-treinta-y-tres.json",
     "./data/nuestra-senora-del-pilar.json",
     "./data/nuestra-senora-de-la-almudena.json",
+    "./data/nuestra-senora-de-los-desamparados.json",
     "./css/style.css",
     "./css/01-base.css",
     "./css/02-layout.css",
