7eb653b32d4f8a7f4419baad736becec77daa8b9
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-24 23:51:54 -0500

    Actualizar caché de la PWA a v71

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v70";
+const CACHE_NAME = "amigos-del-cielo-v71";
 
 const APP_SHELL = [
     "./",
