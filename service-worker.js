4eb7e82dcd9b0e43ced7196f9ab600dc061984a2
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-24 22:35:42 -0500

    Actualizar caché tras corrección de espaciado

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v65";
+const CACHE_NAME = "amigos-del-cielo-v66";
 
 const APP_SHELL = [
     "./",
