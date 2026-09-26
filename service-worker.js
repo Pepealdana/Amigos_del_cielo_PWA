e8b60f6209448c51394ff2814c96efc05d9a85cc
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-25 22:20:32 -0500

    Actualizar caché tras ajustes del catálogo

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v126";
+const CACHE_NAME = "amigos-del-cielo-v127";
 
 const APP_SHELL = [
     "./",
