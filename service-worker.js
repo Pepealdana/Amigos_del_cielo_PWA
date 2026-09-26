b96e705c46d98c19fb33264d6cbc8885f1f9611d
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-26 07:18:02 -0500

    chore: actualizar caché PWA para catálogo y navegación

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v127";
+const CACHE_NAME = "amigos-del-cielo-v128";
 
 const APP_SHELL = [
     "./",
