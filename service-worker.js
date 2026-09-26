5feeb4581456037202999aa26aecf525595be752
pepealdana <115855201+Pepealdana@users.noreply.github.com>
2026-09-25 20:12:05 -0500

    Actualizar caché tras correcciones de estabilidad

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SERVICE WORKER
 ========================================== */
 
-const CACHE_NAME = "amigos-del-cielo-v123";
+const CACHE_NAME = "amigos-del-cielo-v124";
 
 const APP_SHELL = [
     "./",
